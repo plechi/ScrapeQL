fc2af91b13df609846307e8759656cdfc3dd9d85
Plechi <github@plechinger.at>
2015-02-01 21:20:33 +0100

    update constructor

--- src/main/java/at/plechinger/scrapeql/jdbc/ScrapeQLConnection.java
+++ src/main/java/at/plechinger/scrapeql/jdbc/ScrapeQLConnection.java
@@ -47,6 +47,10 @@ import java.util.concurrent.Executor;
  */
 public class ScrapeQLConnection implements java.sql.Connection{
 
+    ScrapeQLConnection(String url, Properties info) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     @Override
     public Statement createStatement() throws SQLException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
